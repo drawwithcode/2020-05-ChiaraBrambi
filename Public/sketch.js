0447eb60d2e1684187ba894e34cfe3efbe53f38c
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-20 23:47:19 +0100

    griglia

--- Public/sketch.js
+++ Public/sketch.js
@@ -12,14 +12,51 @@ function drawOtherMouse(data){
   fill('yellow');
   ellipse(data.x, data.y,20);
 }
+
+var cnv;
+let w, h, num, radius;
+let blackOn, redOn, blueOn, greenOn, yellowOn, purpleOn; //colori
+let colorOn = 0; //colori
+let paper;
+
 function preload(){}
 
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background(120);
+function centerCanvas() {
+var q = (windowWidth - width) / 2;
+var s = (windowHeight - height) / 2;
+cnv.position(q, s);
 }
 
-function draw() {}
+function setup() {
+    cnv = createCanvas(720, 720);
+    centerCanvas();
+    ellipseMode(CORNER);
+    num = 25;
+    h = height / num;
+    w = width / num;
+    radius = 25;
+    background(120);
+
+
+  //colori
+  blackOn = color(0);
+  redOn = color(241, 80, 96);
+  blueOn = color(50, 85, 164);
+  greenOn = color(0, 169, 92);
+  yellowOn = color(255, 181, 17);
+  purpleOn = color(187, 118, 207);
+
+  // extra canvas to draw
+  paper = createGraphics(windowWidth,windowHeight);
+  paper.clear();
+  paper.stroke(207, 244, 255);
+}
+
+function draw() {
+  stroke(207, 244, 255);
+  grid();
+  image(paper, 0, 0);
+}
 
 function mouseMoved(){
   fill('white');
@@ -30,4 +67,18 @@ function mouseMoved(){
   };
 //sand to the server
 socket.emit("mouse", message);
+}
+
+function grid() {
+// questo permette di muoversi SENZA diegnare
+  for (let x = 0; x < width; x += w) {
+    for (let y = 0; y < height; y += h) {
+      if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+        fill(colorOn)
+      } else {
+        fill('white')
+      }
+      rect(x, y, w, h)
+    }
+  }
 }
